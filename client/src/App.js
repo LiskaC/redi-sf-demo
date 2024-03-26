7e452d9e17b4ba874dd2d9ab96a7d4fd256af2bf
Liska Crofts <38409100+LiskaC@users.noreply.github.com>
2024-03-26 18:17:18 +0100

    FE-1: Set up basic react app and add title
    This is a first frontend change.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>demo redi dapp</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
