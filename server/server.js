ae7256e731d8fcb5ce5647038dfce04aa6eae5aa
Liska Crofts <38409100+LiskaC@users.noreply.github.com>
2024-03-26 18:43:24 +0100

    BE-2: Send user data to the frontend
    The frontend is sent the username and password

--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
 import http from 'http'
 
 const server = http.createServer((req, res) => {
+  let requestBody = ''
 
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  req.on('data', (chunk) => {
+    requestBody += chunk;
+  });
+
+  req.on('end', () => {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+
+    const response = {
+        username: 'LISKA',
+        password: 'lol Not Secure'
+    }
+
+    res.end(JSON.stringify(response))
+  })
 })
 
 const PORT = 3000
